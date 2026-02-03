734295be3eac50399d2790213f4026e0a2555359
Maxim Kleynos <maxim.kleynos@guavapay.com>
2026-02-03 17:43:43 +0700

    feat: integrate Google Fonts (Inter) and typography system

---
+++ lib/core/ui-kit/src/theme/typography/base_text_styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract class BaseTextStyles {
+  TextStyle get display;
+
+  TextStyle get h1;
+
+  TextStyle get h2;
+
+  TextStyle get bodyL;
+
+  TextStyle get bodyM;
+
+  TextStyle get bodyS;
+
+  TextStyle get labelS;
+}

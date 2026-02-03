16904db2c9b5edffdf8176c02b623eac75f40ad8
Maxim Kleynos <maxim.kleynos@guavapay.com>
2026-02-03 15:54:38 +0700

    eat: setup navigation architecture and cross-module routing

---
+++ lib/app/router/app_router.gr.dart
@@ -0,0 +1,27 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'app_router.dart';
+
+/// generated route for
+/// [TalkerLoggerScreen]
+class TalkerLoggerRoute extends PageRouteInfo<void> {
+  const TalkerLoggerRoute({List<PageRouteInfo>? children})
+    : super(TalkerLoggerRoute.name, initialChildren: children);
+
+  static const String name = 'TalkerLoggerRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const TalkerLoggerScreen();
+    },
+  );
+}
